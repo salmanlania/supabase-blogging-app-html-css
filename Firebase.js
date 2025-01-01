b294d2ac565723a47476a2d6d6d96948e884ae60
Salman Lania <salmanlania@gmail.com>
2025-01-01 16:58:48 +0500

    second commit

---
+++ Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdJXwDLwneSJQuDLpRT9l2OiAm7TJC61E",
+    authDomain: "smit-projects-59cf9.firebaseapp.com",
+    projectId: "smit-projects-59cf9",
+    storageBucket: "smit-projects-59cf9.firebasestorage.app",
+    messagingSenderId: "585656771043",
+    appId: "1:585656771043:web:7f917579468224872500d6"
+};
+
+const app = initializeApp(firebaseConfig);
